03840aab2495d3eb5a8e6e85f07f614898b36c6c
Messer <9829171+Mes-ser@users.noreply.github.com>
2024-10-17 00:58:54 +0200

    feat: Simple timer.

---
+++ shared/src/core/simple_timer.c
@@ -0,0 +1,35 @@
+#include "core/system.h"
+#include "core/simple_timer.h"
+
+void simple_timer_init(simple_timer_t *timer, uint32_t wait_time, bool auto_reset)
+{
+    timer->wait_time = wait_time;
+    timer->auto_reset = auto_reset;
+    timer->target_time = get_system_ticks() + wait_time;
+    timer->has_elapsed = false;
+}
+bool simple_timer_has_elapsed(simple_timer_t *timer)
+{
+    if (timer->has_elapsed)
+        return false;
+    
+    uint32_t now = get_system_ticks();
+    bool has_elapsed = now >= timer->target_time;
+
+    if (has_elapsed)
+    {
+        if (timer->auto_reset)
+        {
+            timer->target_time += timer->wait_time;
+        }
+        else
+        {
+            timer->has_elapsed = true;
+        }
+    }
+    return has_elapsed;
+}
+void simple_timer_reset(simple_timer_t *timer)
+{
+    simple_timer_init(timer, timer->wait_time, timer->auto_reset);
+}
